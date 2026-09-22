a3c7112e5df5ec65eccefcc698884ce8930a402e
geoffroy-sdp <geoffroymerlin@outlook.fr>
2026-09-22 17:23:11 +0200

    reset portofolio

--- prettier.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config} */
-const config = {
-	useTabs: true,
-	singleQuote: true,
-	jsxSingleQuote: true,
-	singleAttributePerLine: true,
-};
-
-module.exports = config;
